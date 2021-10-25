5c9727d6b8ca5a9268bcc3f7a53863ef32b59a64
kanade-k-1228 <54773334+kanade-k-1228@users.noreply.github.com>
2021-10-26 03:01:30 +0900

    Change scale

--- lib/callback/cpp/mouse_normalize.cpp
+++ lib/callback/cpp/mouse_normalize.cpp
@@ -1,6 +1,6 @@
 #include "../hpp/mouse_normalize.hpp"
 
 void mouse_normalize(int* x, int* y) {
-  *x = *x - glutGet(GLUT_WINDOW_WIDTH) / 2;
-  *y = -*y + glutGet(GLUT_WINDOW_HEIGHT) / 2;
+  *x = 2 * (*x) - glutGet(GLUT_WINDOW_WIDTH);
+  *y = -2 * (*y) + glutGet(GLUT_WINDOW_HEIGHT);
 }
--- lib/callback/cpp/resize.cpp
+++ lib/callback/cpp/resize.cpp
@@ -2,8 +2,8 @@
 
 void resize(const GLsizei width, const GLsizei height) {
   std::cout << "resize\t[" << width << "," << height << "]" << std::endl;
-  glViewport(0, 0, width, height);                                             // ウィンドウ全体をビューポートにする
-  glMatrixMode(GL_PROJECTION);                                                 // 投影変換
-  glLoadIdentity();                                                            // 変換行列の初期化
-  glOrtho(-width / 2.0, width / 2.0, -height / 2.0, height / 2.0, -1.0, 1.0);  // 正投影
+  glViewport(0, 0, width, height);                     // ウィンドウ全体をビューポートにする
+  glMatrixMode(GL_PROJECTION);                         // 投影変換
+  glLoadIdentity();                                    // 変換行列の初期化
+  glOrtho(-width, width, -height, height, -1.0, 1.0);  // 正投影
 }
